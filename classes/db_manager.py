1587c541bab8b9562154deb243a9259a882cc6af
Matheus <matheus.pgua@hotmail.com>
2025-08-26 12:20:08 -0300

    V 0.2.1 - criação do método que preenche as tabelas do db com dados teste

--- classes/db_manager.py
+++ classes/db_manager.py
@@ -15,6 +15,24 @@ class db_manager:
             self.cursor.execute(f"""INSERT INTO {table_name} VALUES {value_string}""")
         self.connection.commit()
 
+    def test_values(self):
+        self.insert_values('users',[f"('akuma','akuma','akuma@akuma','akuma','player')"])
+        self.insert_values('users',[f"('ken','ken','ken@ken','ken','player')"])
+        self.insert_values('users',[f"('cammy','cammy','cammy@cammy','cammy','dm')"])
+        self.insert_values('users',[f"('guile','guile','guile@guile','guile','player&dm')"])
+        self.insert_values('players',[f"('{self.get_user_id_by_username('akuma')}')"])
+        self.insert_values('players',[f"('{self.get_user_id_by_username('guile')}')"])
+        self.insert_values('players',[f"('{self.get_user_id_by_username('ken')}')"])
+        self.insert_values('dms',[f"('{self.get_user_id_by_username('guile')}')"])
+        self.insert_values('dms',[f"('{self.get_user_id_by_username('cammy')}')"])
+        self.insert_values('magic_items',[f"('Bead of Fireballs', 'Fireballs!!!!', '50000')"])
+        self.insert_values('magic_items',[f"('Hand of Vecna', 'Eugh...', 'Not for sale')"])
+        self.insert_values('characters',[f"('{self.get_user_id_by_username('akuma')}','Draco','Fighter','Arcane Archer','Dragonborn','1','0','300')"])
+        self.insert_values('characters',[f"('{self.get_user_id_by_username('guile')}','Melf','Wizard','Evocation','Elf','5','6500','9000')"])
+        self.insert_values('characters',[f"('{self.get_user_id_by_username('ken')}','John','Barbarian','Giant','Human','3','900','3000')"])
+        self.insert_values('campaigns',[f"('Avernus', 'Hell all over the place', 'https://roll20.net/', '{self.get_user_id_by_username('cammy')}','({self.get_user_id_by_username('guile')},{self.get_user_id_by_username('akuma')})',('({self.get_character_id_by_name('Melf')},{self.get_character_id_by_name('Draco')})'),'0','{self.get_magicitem_id_by_name('Bead of Fireballs')}','active')"])
+        self.insert_values('campaigns',[f"('Dragons', 'Dragons for fucks sake', 'https://roll20.net/', '{self.get_user_id_by_username('guile')}','({self.get_user_id_by_username('ken')},{self.get_user_id_by_username('akuma')})',('({self.get_character_id_by_name('John')},{self.get_character_id_by_name('Draco')})'),'0','{self.get_magicitem_id_by_name('Hand of Vecna')}','finished')"])
+
     def delete_all(self):
         self.cursor.execute('DROP TABLE characters')
         self.cursor.execute('DROP TABLE players')
@@ -34,8 +52,9 @@ class db_manager:
         self.create_table('magic_items','(name, description, price)')
         self.create_table('users','(username, password, email, name, type)')
         self.create_table('posts','(user_ID, campaign_ID, subject, message, created_at)')
-        self.create_table('campaigns','(title, description, game_link, dm_ID, players_IDs, posts_IDs, magic_items_IDs, current_status)')
+        self.create_table('campaigns','(title, description, game_link, dm_ID, players_IDs, characters_IDs, posts_IDs, magic_items_IDs, current_status)')
         self.create_table('log','(date, time, table_name, user_id, operation_description)')
+        self.test_values()
 
     def check_username(self, informedUsername, informedPassword):
 #        print(self.cursor.execute(f'SELECT username, password FROM users WHERE username = "{informedUsername}" AND password = "{informedPassword}"').fetchall())
@@ -43,8 +62,23 @@ class db_manager:
     
     def get_user_name_by_username(self, informedUsername):
         return self.cursor.execute(f'SELECT name FROM users WHERE username = "{informedUsername}"').fetchall()[0][0]
+    
+    def get_user_id_by_username(self, informedUsername):
+        if(self.cursor.execute(f'SELECT rowid FROM users WHERE username = "{informedUsername}"').fetchall()):
+            return self.cursor.execute(f'SELECT rowid FROM users WHERE username = "{informedUsername}"').fetchall()[0][0]
+        return ''
+    
+    def get_magicitem_id_by_name(self, informedName):
+        if(self.cursor.execute(f'SELECT rowid FROM magic_items WHERE name = "{informedName}"').fetchall()):
+            return self.cursor.execute(f'SELECT rowid FROM magic_items WHERE name = "{informedName}"').fetchall()[0][0]
+        return ''
 
-#db_manager().reset_all()
+    def get_character_id_by_name(self, informedName):
+        if(self.cursor.execute(f'SELECT rowid FROM characters WHERE name = "{informedName}"').fetchall()):
+            return self.cursor.execute(f'SELECT rowid FROM characters WHERE name = "{informedName}"').fetchall()[0][0]
+        return ''
+
+# db_manager().reset_all()
 #db_manager().insert_values('users',[f"('math_user','math_pass', 'math_email','math','dm')"])
 
 #####
