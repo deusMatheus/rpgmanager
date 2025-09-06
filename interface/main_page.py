9bd46496a925772701e6071f266179e55186836b
Matheus <matheus.pgua@hotmail.com>
2025-09-06 12:04:43 -0300

    Adicionar itens na main page

--- interface/main_page.py
+++ interface/main_page.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.write('Página principal - Versão 0.5.1')
+st.title('W.I.P')
+st.write('- Adicionar imagens aos personagens')
+st.write('- Separar jogadores e personagens nas páginas de campanha')
+st.write('- Criar sistema de posts para campanha')
+st.write('- Criar linha do tempo')
