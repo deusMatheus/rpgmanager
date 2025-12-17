d67f2d6909854f3852b27d7d4cad1948af067a78
Matheus <matheus.pgua@hotmail.com>
2025-12-16 23:11:19 -0300

    V0.6 - nova ideia de journal, já contém criação de contas. Olhar o texto em main_page.py

--- interface/main_page.py
+++ interface/main_page.py
@@ -6,3 +6,15 @@ st.write('- Adicionar imagens aos personagens')
 st.write('- Separar jogadores e personagens nas páginas de campanha')
 st.write('- Criar sistema de posts para campanha')
 st.write('- Criar linha do tempo')
+
+st.title('Ideia diferente...')
+st.write('Ao invés de fazer com personagens e etc, fazer um sistema de journal em que:')
+st.write('1 - Todo mundo é livre pra criar uma campanha, sendo colocado automaticamente como DM')
+st.write('2 - Todo DM pode adicionar jogadores à suas campanhas')
+st.write('3 - Todos os participantes da campanha (DMs e PLAYERs) podem criar EVENTOS na campanha.')
+st.write('4 - Colocar um botão para adicionar divisória e outro para removê-la.')
+st.write('5 - Nos eventos adicionados opta-se por adicionar data. Assim uma data pode ser adicionada logo após uma divisória para organizar melhor os eventos.' \
+'Deve-se ainda colocar uma breve descrição; ex "pedra 50PO e 100XP combate surpresa com os cogumelos sebosos." ')
+st.write('6 - Isso vai dar um trabalho do cacete, porque será necessário remodelar o DB. Pode-se aproveitar a estrutura das tabelas atuais para criar as novas.' \
+'Ou talves apenas renomear tabelas e colunas.')
+st.write('7 - Começar criando uma branch no git/github. Após, iniciar removendo a "Página dos jogadores" ')
